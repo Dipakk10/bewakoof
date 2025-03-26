8318564b7467ebd55fab14e48eedfc964fda4b1e
Dipak kumar kewat <dipak8581987732@gmail.com>
2025-03-26 19:21:55 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import './App.css'
 import Navbar from './Component/Frontend/Headeer/Navbar'
+import Slider from './Component/Frontend/Top_Sliders/Slider'
 
 function App() {
 
   return (
    <>
     <Navbar />
+    <Slider images={images} />
+    <Slider images={images2} />
    </>
   )
 }
 
 export default App
+const images =[
+  {name:'asset 1.gif', alt:'asset 1'},
+  {name:'asset 2.webp', alt:'asset 2'},
+  {name:'asset 3.webp', alt:'asset 3'},
+  {name:'asset 4.webp', alt:'asset 4'},
+  {name:'asset 5.webp', alt:'asset 5'},
+  {name:'asset 6.webp', alt:'asset 6'},
+  {name:'asset 7.webp', alt:'asset 7'},
+  {name:'asset 8.webp', alt:'asset 8'},
+  {name:'asset 9.webp', alt:'asset 9'},
+]
+const images2 =[
+  {name:'asset 11.webp', alt:'asset 1'},
+  {name:'asset 12.webp', alt:'asset 2'},
+  {name:'asset 13.webp', alt:'asset 3'},
+  {name:'asset 14.webp', alt:'asset 4'},
+  {name:'asset 15.webp', alt:'asset 5'},
+  {name:'asset 16.webp', alt:'asset 6'},
+  {name:'asset 17.webp', alt:'asset 7'},
+  {name:'asset 18.webp', alt:'asset 8'},
+  {name:'asset 19.webp', alt:'asset 9'},
+]
--- src/Component/Frontend/Headeer/Navbar.jsx
+++ src/Component/Frontend/Headeer/Navbar.jsx
@@ -1,80 +1,99 @@
 import React from 'react';
 import './Navbar.css'
+import { useNavigate } from "react-router-dom";
 
 // import img from "../../../Image/Header/mobile.svg"
 
 const Navbar = () => {
-  return (
-    <>
-      <div className='topnav'>
-        <div className='topnav-1'>
-          <div className='topnav_1a'>
-            <span>Offers</span>
-            <span>Facebook</span>
-            {/* <img src={img} alt="Mobile" width={"100px"} height={"100px"} /> */}
-            <div className='imgage'>
-              <img src="/Header/mobile.svg" alt="Mobile Icon" />
-              <span>Download App</span>
+
+  // const navigate = useNavigate();
+
+  // const handleRedirect = () => {
+  //   navigate("/Man");
+  //   navigate("/WoMan"); 
+  //   navigate("/Live_Now"); 
+  //   navigate("/Pluse_Size"); 
+  //   navigate("/Accessories"); 
+  //   navigate("/Official_Merch"); 
+  //   navigate("/Sneakers");
+  //   navigate("/Bewakoof_Air");
+  //   navigate("/Heavy Duty");
+  //   navigate("/Customization");
+  //   navigate("/Winterwear");
+    
+  // };
+
+return (
+  <>
+    <div className='topnav'>
+      <div className='topnav-1'>
+        <div className='topnav_1a'>
+          <span>Offers</span>
+          <span>Facebook</span>
+          {/* <img src={img} alt="Mobile" width={"100px"} height={"100px"} /> */}
+          <div className='imgage'>
+            <img src="/Header/mobile.svg" alt="Mobile Icon" />
+            <span>Download App</span>
+          </div>
+
+          <span>Find a store near me</span>
+        </div>
+        <div className='topnav_1b'>
+          <span>Contact Us</span>
+          <span>Track Trder</span>
+        </div>
+      </div>
+
+      <div className="topnav-2">
+        <div className='topnav-2z'>
+          <div className='topnav_2y'>
+            <img src="/Header/Bewakooflogo.svg" alt="logo" />
+            <div className='topnav_2a'>
+              <a href="#">MEN</a>
+              <a href="#">WOMEN</a>
+              <a href="#">MOBILE COVER</a>
+            </div>
+          </div>
+
+          <div className='topnav_2b'>
+
+            <div class="form-group has-search">
+              <img src="/Header/search icon.svg" alt="Search" className='form-control-feedback' />
+              <input type="text" class="form-control" placeholder="Search" />
             </div>
 
-            <span>Find a store near me</span>
-          </div>
-          <div className='topnav_1b'>
-            <span>Contact Us</span>
-            <span>Track Trder</span>
+            <div className='topnav_2c'>
+              <span>|</span >
+              <p>LOGIN</p>
+              <img src="/Header/Heart.svg" alt="favourte" />
+              <img src="/Header/Bag.svg" alt="cart" />
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className="topnav-2">
-          <div className='topnav-2z'>
-            <div className='topnav_2y'>
-              <img src="/Header/Bewakooflogo.svg" alt="logo" />
-              <div className='topnav_2a'>
-                <a href="#">MEN</a>
-                <a href="#">WOMEN</a>
-                <a href="#">MOBILE COVER</a>
-              </div>
-            </div>
-
-            <div className='topnav_2b'>
-
-              <div class="form-group has-search">
-                <img src="/Header/search icon.svg" alt="Search" className='form-control-feedback' />
-                <input type="text" class="form-control" placeholder="Search" />
-              </div>
-
-              <div className='topnav_2c'>
-                <span>|</span >
-                <p>LOGIN</p>
-                <img src="/Header/Heart.svg" alt="favourte" />
-                <img src="/Header/Bag.svg" alt="cart" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="topnav-3">
-          <div className="topnav_3a">
-            <p><a href='#'>MEN</a></p>
-            <p><a href='#'>WOMEN</a></p>
-            <p><a href='#'>PLUS SIZE</a></p>
-            <p><a href='#'>ACCESSORISE</a></p>
-            <p><a href='#'>OFFICIAL MERCH</a></p>
-            <p><a href='#'>SNEAKERS</a></p>
-            <p><a href='#'>BEWAKOOF AIR</a></p>
-            <p><a href='#'>HEAVY DUTY</a></p>
-            <p><a href='#'>CUSTOMIZATION</a></p>
-            <p><a href='#'>WINTERWEAR</a></p>
-          </div>
-
+      <div className="topnav-3">
+        <div className="topnav_3a">
+          <p><a href='#'>MEN</a></p>
+          <p><a href='#'>WOMEN</a></p>
+          <p><a href='#'>PLUS SIZE</a></p>
+          <p><a href='#'>ACCESSORISE</a></p>
+          <p><a href='#'>OFFICIAL MERCH</a></p>
+          <p><a href='#'>SNEAKERS</a></p>
+          <p><a href='#'>BEWAKOOF AIR</a></p>
+          <p><a href='#'>HEAVY DUTY</a></p>
+          <p><a href='#'>CUSTOMIZATION</a></p>
+          <p><a href='#'>WINTERWEAR</a></p>
         </div>
 
       </div>
 
+    </div>
 
 
-    </>
-  )
+
+  </>
+)
 }
 
-export default Navbar
+export default Navbar;
